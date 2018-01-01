7e36aa5dddbad1ded32570dae11bf1bd36f6e132
Saeed Nejati <snejati@uwaterloo.ca>
2018-01-01 00:34:13 -0500

    Adding echo command and updating whoami link

--- shell/process.php
+++ shell/process.php
@@ -1,6 +1,7 @@
 <?php
 $command = $_GET['command'];
-switch($command)
+$cmd = explode(" ", $command);
+switch($cmd[0])
 {
 case "help" :
 	echo "<table border='0' cellpadding='2' cellspacing='0'>".
@@ -8,6 +9,7 @@ case "help" :
 		"<tr><td width='150px'><b><i>clear</b></i></td><td>Clears the screen..</td></tr>".
 		"<tr><td width='150px'><b><i>ls</b></i></td><td>Prints a list of files..</td></tr>".
 		"<tr><td width='150px'><b><i>whoami</b></i></td><td>Prints a little information about me..</td></tr>".
+		"<tr><td width='150px'><b><i>echo</b></i></td><td>Prints back the given string..</td></tr>".
 		"</table>";
 	break;
 case "ls" :
@@ -15,12 +17,17 @@ case "ls" :
 	break;
 case "whoami" :
 	echo "<table border='0' cellpadding='2' cellspacing='0'>".
-		"<tr><td width='150px'><b>Name</b></td><td><a href='http://nejati.googlepages.com'>Saeed Nejati</a></td></tr>".
+		"<tr><td width='150px'><b>Name</b></td><td><a href='http://ece.uwaterloo.ca/~snejati'>Saeed Nejati</a></td></tr>".
 		"<tr><td width='150px'><b>Education</b></td><td>PhD student @ University of Waterloo</td></tr>".
 		"<tr><td width='150px'><b>Birth Date</b></td><td>10 Jan 1984</td></tr>".
 		"<tr><td width='150px'><b>E-Mail</b></td><td>nejati@gmail.com</td></tr>".
 		"</table>";
-	break;
+    break;
+case "echo":
+    $cp_cmd = $cmd;
+    unset($cp_cmd[0]);
+    echo "<pre>" . implode(" ", $cp_cmd) . "</pre>";
+    break;
 default :
 	echo "Unknown Command.. <br>";
 }
