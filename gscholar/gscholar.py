78217e83058554eed283f7024227de65894bece1
Saeed Nejati <nejati@gmail.com>
2021-07-22 17:03:18 -0700

    adding timestamp and dumping to a file

--- gscholar/gscholar.py
+++ gscholar/gscholar.py
@@ -1,6 +1,8 @@
 import urllib.request
 from html.parser import HTMLParser
 import json
+from datetime import datetime
+import os
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
@@ -61,10 +63,19 @@ parser = MyHTMLParser()
 with open("users.json", "r") as f:
     users = json.load(f)
 
-print("name, citations, h_index, i10_index")
-for name, user_id in users.items():
-    url = get_url(user_id)
-    content = urllib.request.urlopen(url).read()
-    parser.feed(content.decode("iso-8859-1"))
-    citations, h_index, i10_index = parser.get_stat()
-    print(f"{name}, {citations}, {h_index}, {i10_index}")
+print_header = False
+if not os.path.exists("stat.csv"):
+    print_header = True
+
+with open("stat.csv", "a") as f:
+    if print_header:
+        f.write("name, date, citations, h_index, i10_index\n")
+    now = datetime.now()
+    date_str = now.strftime("%d-%m-%Y-%H:%M")
+    for name, user_id in users.items():
+        url = get_url(user_id)
+        content = urllib.request.urlopen(url).read()
+        parser.feed(content.decode("iso-8859-1"))
+        citations, h_index, i10_index = parser.get_stat()
+
+        f.write(f"{name}, {date_str}, {citations}, {h_index}, {i10_index}\n")
